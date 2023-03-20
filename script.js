a2aced6ef01bf894e9dadaf1575bf7815698cb21
valeroman <valeraromanovo@gmail.com>
2023-03-20 16:39:46 +0300

    Fix show/hide input div

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-const INPUT_BUTTON = document.getElementById('input-button');
-INPUT_BUTTON.onclick = addBookToLibrary(author, title, pagesNum);
+let myLibrary = [];
 
 const OUTPUT_TABLE = document.getElementById('table-output');
 
-let myLibrary = [];
+const INPUT_FORM = document.getElementById('input-form');
+
+const INPUT_BUTTON = document.getElementById('input-button');
+INPUT_BUTTON.onclick = function (e) {
+  e.preventDefault();
+  switchForm();
+};
+
+const SUBMIT_BUTTON = document.getElementById('submit-button');
+SUBMIT_BUTTON.onclick = function (e) {
+  e.preventDefault();
+  addBookToLibrary(author, title, pagesNum);
+  switchForm();
+};
+
 
 function Book(author, title, pagesNum) {
   this.author = author;
@@ -13,6 +26,7 @@ function Book(author, title, pagesNum) {
 
 function addBookToLibrary(author, title, pagesNum) {
   myLibrary.push(new Book(author, title, pagesNum));
+  showBooksTable(myLibrary);
 }
 
 function showBooksTable(myLibrary) {
@@ -26,4 +40,10 @@ function showBooksTable(myLibrary) {
     }
   }
   OUTPUT_TABLE.appendChild(ROW);
+}
+
+function switchForm() {
+  INPUT_FORM.className === 'input-form hidden'
+  ? INPUT_FORM.className = 'input-form'
+  : INPUT_FORM.className = 'input-form hidden';
 }
